cb92e658488cd1e922e73ebfa4808de188db5f61
TheRiseOfDavid <david53133@gmail.com>
2021-03-09 23:54:30 +0800

    20210309.2354

--- 11402/main.cpp
+++ 11402/main.cpp
@@ -1,151 +1,140 @@
-#include <iostream>
 #include <bits/stdc++.h>
 #define LOCAL
 #define MAXN 1024200
-#define lson(x) x<<1
-#define rson(x) (x<<1) + 1
+#define Lson(x) x<<1
+#define Rson(x) (x<<1)+1
 using namespace std;
+int t, n, m, a, b, Qcnt, q, kase, len;
 int num[MAXN];
-int t, n, m, q, cnt, a , b, kase=1, qcnt;
-string temp;
-struct{
-    int l, r, v, z1, z2;
-
+struct tree{
+    int l, r, v, z;
     void print(){
-        cout << "node l r v z1 z2 " << l << ' ' << r << ' ' << v << ' ' << z1 << ' ' << z2 << '\n';
+        cout << "l = " << l << " r = " << r << " v = " << v << " z = " << z << '\n';
     }
 }node[MAXN*4];
 
 void build(int l, int r, int x = 1){
+
+    //cout << "x = " << x << " l = " << l << " r = " << r << '\n';
     node[x].l = l;
     node[x].r = r;
-    node[x].z1 = 0;
-    node[x].z2 = 0;
+    node[x].z = -1;
     if(l == r){
         node[x].v = num[l];
         return;
     }
 
-    int mid = (l + r) / 2;
-    build(l, mid, lson(x));
-    build(mid+1, r, rson(x));
-    node[x].v = max(node[lson(x)].v, node[rson(x)].v);
+    int mid = (l+r)/2;
+    build(l, mid, Lson(x));
+    build(mid+1, r, Rson(x));
+    node[x].v = node[Lson(x)].v + node[Rson(x)].v;
 }
 
-void push(int x){
-    int i;
-    if(node[x].z2 != 0 ){
-        node[lson(x)].z2 = node[x].z2;
-        node[rson(x)].z2 = node[x].z2;
-        node[x].z2 = 0;
+void mod1(int x, int cmd){
+    int lson = Lson(x), rson = Rson(x);
+    node[lson].z = cmd;
+    node[rson].z = cmd;
 
-        node[lson(x)].v = (node[lson(x)].r-node[lson(x)].l) - node[lson(x)].v;
-        node[rson(x)].v = (node[rson(x)].r-node[rson(x)].l) - node[rson(x)].v;
-    }
-    if(node[x].z1 != 0 ){
-        node[lson(x)].z1 += node[x].z1;
-        node[rson(x)].z1 += node[x].z1;
-        node[x].z1 = 0;
-
-        node[lson(x)].v = (node[lson(x)].r-node[lson(x)].l) * node[lson(x)].z1;
-        node[rson(x)].v = (node[rson(x)].r-node[rson(x)].l) * node[rson(x)].z1;
-    }
+    node[lson].v = (node[lson].r-node[lson].l+1) * cmd;
+    node[rson].v = (node[rson].r-node[rson].l+1) * cmd;
 }
 
-void interMod1(int a, int b, int m, int x = 1){
-    /*
-    cout << "x a b " << x << ' ' << a << ' ' << b << ' ' << "interMod1" << "\n";
-    node[x].print();
-    cout << '\n';
-    */
+void mod2(int x, int cmd){
+    int lson = Lson(x), rson = Rson(x);
 
-    if(a <= node[x].l && b >= node[x].r){ //this node interval will be recover
-        node[x].v = (node[x].r-node[x].l) * m; //m is 1 or 0
-        node[x].z1 += m;
+    if(node[lson].z == 2) node[lson].z = -1;
+    else if(node[lson].z != -1) node[lson].z ^= 1;
+    else node[lson].z = cmd;
+
+    if(node[rson].z == 2) node[rson].z = -1;
+    else if(node[rson].z != -1) node[rson].z ^= 1;
+    else node[rson].z = cmd;
+
+    node[lson].v = (node[lson].r-node[lson].l+1) - node[lson].v;
+    node[rson].v = (node[rson].r-node[rson].l+1) - node[rson].v;
+}
+
+void push_down(int x){
+    if(node[x].z == 0) mod1(x, 0);
+    if(node[x].z == 1) mod1(x, 1);
+    if(node[x].z == 2) mod2(x, 2);
+    node[x].z = -1;
+}
+
+void push_mod1(int a, int b, int cmd, int x = 1){
+    if(a <= node[x].l && b >= node[x].r){
+        node[x].v = (node[x].r-node[x].l+1) * cmd;
+        node[x].z = cmd;
         return;
     }
-    push(x);
+    push_down(x);
 
-    int mid = (node[x].l+node[x].r)/2;
-    if(b <= mid) interMod1(a, b, m, lson(x));
-    if(a > mid) interMod1(a, b, m, rson(x));
-    node[x].v = node[lson(x)].v + node[rson(x)].v;
+    int mid = (node[x].l+node[x].r) / 2;
+    if(a <= mid) push_mod1(a, b, cmd, Lson(x));
+    if(b > mid) push_mod1(a, b, cmd, Rson(x));
+    node[x].v = node[Lson(x)].v + node[Rson(x)].v;
 }
 
-void interMod2(int a, int b, int m, int x = 1){
-    /*
-    cout << "x a b " << x << ' ' << a << ' ' << b << ' ' << "interMod2" << "\n";
-    node[x].print();
-    cout << '\n';
-    */
+void push_mod2(int a, int b, int cmd, int x = 1){
+    if(a <= node[x].l && b >= node[x].r){
+        node[x].v = (node[x].r-node[x].l+1) - node[x].v;
 
-    if(a <= node[x].l && b >= node[x].r){ //this node interval will be recover
-        node[x].v = (node[x].r-node[x].l) - node[x].v; //m is 1 or 0
-        node[x].z2 += m;
+        if(node[x].z == 2) node[x].z = -1;
+        else if (node[x].z != -1) node[x].z ^= 1;
+        else node[x].z = cmd;
+
         return;
     }
-    push(x);
+    push_down(x);
 
-    int mid = (node[x].l+node[x].r)/2;
-    if(a <= mid) interMod2(a, b, m, lson(x));
-    if(b > mid) interMod2(a, b, m, rson(x));
-    node[x].v = node[lson(x)].v + node[rson(x)].v;
+    int mid = (node[x].l+node[x].r) / 2;
+    if(a <= mid) push_mod2(a, b, cmd, Lson(x));
+    if(b > mid) push_mod2(a, b, cmd, Rson(x));
+    node[x].v = node[Lson(x)].v + node[Rson(x)].v;
 }
 
 int query(int l, int r, int x = 1){
-    //cout << "query x l r " << x << ' ' << l << ' ' << r << '\n';
-    //node[x].print();
-
     if(node[x].l >= l && node[x].r <= r) return node[x].v;
-    push(x);
+    push_down(x);
 
     int mid = (node[x].l+node[x].r) / 2, ans = 0;
-    if(l <= mid) ans += query(l, r, lson(x));
-    if(r > mid) ans += query(l, r, rson(x));
+    if(l <= mid) ans += query(l, r, Lson(x));
+    if(r > mid) ans += query(l, r, Rson(x));
+    //cout << "q x = " << x << " ans = " << ans << '\n';
     return ans;
 }
 
-void debug_num(){
-    for(int i = 0; i < cnt; i++) cout << num[i];
-    cout << '\n';
-}
 
-void debug_node(){
-    for(int i = 1; i < 15; i++) node[i].print();
-}
-
-int main()
-{
+int main(){
 #ifdef LOCAL
     freopen("in1.txt", "r", stdin);
 #endif // LOCAL
+    string temp;
     cin >> t;
     while(t--){
-        qcnt = 1;
-        cout << "Case " << kase++ << ":\n";
+        Qcnt = 1;
+        cout << "Case " << ++kase << ":\n";
         cin >> n;
-        cnt = 0;
+        len = 1;
         while(n--){
             cin >> m >> temp;
             for(int i = 0; i < m; i++){
-                for(int j = 0; j < temp.length(); j++) num[cnt++] = temp[j] - '0';
+                for(int j = 0; j < temp.length(); j++) num[len++] = temp[j] - '0';
             }
         }
-        //debug_num();
 
-        build(1, cnt);
+        build(1, len);
         cin >> q;
         for(int i = 0; i < q; i++){
             cin >> temp >> a >> b;
             a++; b++; //index start from 1
-            if(temp == "F") interMod1(a, b, 1);
-            else if(temp == "E") interMod1(a, b, 0);
-            else if(temp == "I") interMod2(a, b, 1);
-            else if(temp == "S") cout << "Q" << qcnt++ << ": " << query(a, b) << "\n";
-            debug_node();
-            cout << '\n';
-        }
+            if(temp == "F") push_mod1(a, b, 1);
+            else if(temp == "E") push_mod1(a, b, 0);
+            else if(temp == "I") push_mod2(a, b, 2);
+            else if(temp == "S") cout << "Q" << Qcnt++ << ": " << query(a, b) << "\n";
 
+        }
     }
     return 0;
 }
+
